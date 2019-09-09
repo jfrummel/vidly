a196a0c2a7682927a1d00d9985874eb3de4294c0
jfrummel <jfrummel@gmail.com>
2019-09-09 16:42:35 -0500

    Password hashing

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Joi = require("joi");
 Joi.objectId = require("joi-objectid")(Joi);
 const mongoose = require('mongoose');
 const customers = require('./routes/customers');
+const users = require("./routes/users");
 const genres = require('./routes/genres');
 const movies = require("./routes/movies");
 const rentals = require("./routes/rentals");
@@ -17,6 +18,7 @@ app.use('/api/customers', customers);
 app.use('/api/genres', genres);
 app.use("/api/movies", movies);
 app.use("/api/rentals", rentals);
+app.use("/api/users", users);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
--- models/users.js
+++ models/users.js
@@ -1,1 +1,38 @@
-const
+const mongoose = require("mongoose");
+const Joi = require("joi");
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        minlength: 3,
+        maxlength: 255,
+        required: true
+    },
+    email: {
+        type: String,
+        minlength: 5,
+        maxlength: 255,
+        unique: true,
+        required: true
+    },
+    password: {
+        type: String,
+        minlength: 8,
+        maxlength: 1024,
+        required: true
+    }
+});
+
+const User = mongoose.model("User", userSchema);
+
+function validateUser(user) {
+    const schema = {
+        name: Joi.string().min(3).max(255).required(),
+        email: Joi.string().min(3).max(255).required().email(),
+        password: Joi.string().min(8).max(255).required()
+    }
+    return Joi.validate(user, schema);
+};
+
+exports.User = User;
+exports.validate = validateUser;
